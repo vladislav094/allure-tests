4f8b2a6cf87404ac1fc1afed7f41c264766c74af
vladislav094 <vkhrabrov94@gmail.com>
2022-02-14 22:58:31 +0300

    first

--- test_1.py
+++ test_1.py
@@ -12,7 +12,7 @@ def browser():
     driver.implicitly_wait(1)
     yield
     driver.quit()
-    print('Test comletede')
+    print('Test comletedse')
 
 @allure.feature('Check')
 @allure.story('Open site and type credentials')
